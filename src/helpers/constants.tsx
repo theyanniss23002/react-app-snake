c0ea255fcdbae6993b8b32851d4feef0c8067ecb
The Yanniss <theyanniss@MacBook-Pro-The.local>
2021-11-16 11:47:23 +0300

    Changed size area

--- src/helpers/constants.tsx
+++ src/helpers/constants.tsx
@@ -1,11 +1,11 @@
-export const CANVAS_SIZE = { x: 800, y: 800 };
+export const CANVAS_SIZE = { x: 500, y: 500 };
 export const SNAKE_START = [
     { x: 8, y: 7 },
     { x: 8, y: 8 }
 ];
 export const DIRECTION_START = { x: 0, y: -1 };
 export const APPLE_START = { x: 8, y: 3 };
-export const SCALE = 40;
+export const SCALE = 25;
 export const SPEED = 200;
 export const MAX_POINTS = 100;
 export const DIRECTIONS = {
